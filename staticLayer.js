e4eb87db6d38d2dad6cb064f5871098ef2f6188c
m0ose <cody@simtable.com>
2016-03-11 13:44:39 -0700

    i guess it works

--- staticLayer.js
+++ staticLayer.js
@@ -10,45 +10,37 @@ L.StaticLayer = L.ElementOverlay.extend({
     this._url = url;
     this._bounds = L.latLngBounds([0,0],[1,1])// the are not going to be used
     L.setOptions(this, options);
-    //'resize',
-    //            'move',
-    //            'moveend'
-
   },
-  /*reposition: function () {
-    var pos = this.map._getMapPanePos().multiplyBy(-1);
-    L.DomUtil.setPosition(this._image, pos);
-  },
-  */
-  /*onAdd: function(map) {
-    console.log('onadd called', arguments)
-    map.on('resize', this._moveEventFired, this)
-    map.on('move', this._moveEventFired, this)
-    map.on('moveend', this._moveEventFired, this)
-  },*/
 
   _moveEventFired: function (e) {
-    console.log('move event fired')
     this._reset()
   },
 
   _animateZoom: function (e) {
-    console.log('animate zoom called')
     this._reset()
   },
 
   eventsRegistered:false,
 
   _reset: function () {
-    console.log('reset called')
     var pos = this._map._getMapPanePos().multiplyBy(-1);
     L.DomUtil.setTransform(this.getElement(), pos, 1)
+    this.registerMyEvents.bind(this)()
+  },
+
+  registerMyEvents: function (onoff) {
     if(!this.eventsRegistered) {
       console.log('registered events')
       this.eventsRegistered = true
-      map.on('resize', this._moveEventFired, this)
-      map.on('move', this._moveEventFired, this)
-      map.on('moveend', this._moveEventFired, this)
+      this._map.on('resize', this._moveEventFired, this)
+      this._map.on('move', this._moveEventFired, this)
+      this._map.on('moveend', this._moveEventFired, this)
     }
-  },
+    if(onoff == 'off') {
+      this._map.off('resize', this._moveEventFired, this)
+      this._map.off('move', this._moveEventFired, this)
+      this._map.off('moveend', this._moveEventFired, this)
+      this.eventsRegistered = false
+    }
+  }
 });
